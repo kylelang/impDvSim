f3957183f7f37f88db141a642a107248ab318db7
Kyle M. Lang <kylelang@debianNotebook>
2019-11-07 07:25:05 +0100

    more work on missing data simulation methods

--- code/.sandbox/mdSimTesting.R
+++ code/.sandbox/mdSimTesting.R
@@ -1,15 +1,23 @@
 ### Title:    Experiments with Missing Data Simulation Methods
 ### Author:   Kyle M. Lang
 ### Created:  2019-11-06
-### Modified: 2019-11-06
+### Modified: 2019-11-07
 
 
 ### ToDo:
 ###   Figure out why the 'center' type is producing too much missingness
 
-fAlpha <- function(alpha, eta, pm) (mean(plogis(alpha + eta)) - pm)^2
+fAlpha <- function(alpha, eta, pm, filter = rep(TRUE, length(eta))) {
+    p <- plogis(alpha + eta)
+    r <- as.logical(rbinom(n = length(p), size = 1, prob = p))
+    
+    (mean(r & filter) - pm)^2
+}
 
-optAlpha <- function(pm, eta, tol = c(0.1, 0.001), maxIter = 10) {
+
+                                        #(mean(plogis(alpha + eta)) - pm)^2
+
+optAlpha <- function(pm, eta, tol = c(0.1, 0.001), maxIter = 10, filter = rep(TRUE, length(eta))) {
     for(k in 1 : maxIter) {
         ## Define the search range:
         int <- k * range(eta)
@@ -18,7 +26,8 @@ optAlpha <- function(pm, eta, tol = c(0.1, 0.001), maxIter = 10) {
         out <- optimize(f        = fAlpha,
                         interval = int,
                         eta      = eta,
-                        pm       = pm)
+                        pm       = pm,
+                        filter   = filter)
         
         ## Are we far enough from the boundary?
         dist   <- out$minimum - int
@@ -41,11 +50,11 @@ optAlpha <- function(pm, eta, tol = c(0.1, 0.001), maxIter = 10) {
     out
 }
 
-rVec <- function(pm, eta, alpha = NULL) {
+rVec <- function(pm, eta, alpha = NULL, filter = rep(TRUE, length(eta))) {
     if(is.null(alpha))
-        alpha <- optAlpha(pm = pm, eta = eta)$minimum
+        alpha <- optAlpha(pm = pm, eta = eta, filter = filter)$minimum
     
-    probs  <- plogis(alpha + eta)
+    probs <- plogis(alpha + eta)
 
     list(rVec  = as.logical(rbinom(n = length(eta), size = 1, prob = probs)),
          alpha = alpha)
@@ -62,7 +71,37 @@ rVec <- function(pm, eta, alpha = NULL) {
 data  <- dat0
 preds <- colnames(dat0)
 beta  <- rep(1.0, length(preds))
-pm    <- 0.2
+pm    <- 0.5
+type  <- "tails"
+
+formula <- switch(type[i],
+                  LEFT = function(x, b) logit(mean(x) - x + b),
+                  MID = function(x, b) logit(-abs(x - mean(x)) + 0.75 + b),
+                  TAIL = function(x, b) logit(abs(x - mean(x)) - 0.75 + b),
+                  function(x, b) logit(-mean(x) + x + b))
+
+x <- rnorm(n, 10, 5)
+
+b <- 0.5
+
+y <- x - mean(x) + b
+y <- mean(x) - x + b
+
+y <- -abs(x - mean(x)) + b
+y <- abs(x)
+
+plot(x = x, y = plogis(y))
+
+?rbeta
+
+n <- 10000
+a <- 0.9
+b <- 0.9
+
+x <- seq(0, 1, length.out = n)
+y <- dbeta(x, a, b)
+
+plot(x = x, y = y, type = "l")
 
 simMissingness <- function(pm,
                            data,
@@ -73,6 +112,22 @@ simMissingness <- function(pm,
     if(is.null(beta)) beta <- rep(1.0, length(preds))
     
     eta <- as.matrix(data[ , preds]) %*% matrix(beta)
+
+    eta <- rnorm(n)
+    plot(x = eta, y = plogis(eta))
+
+    eta <- abs(eta - mean(eta))
+    plot(x = eta, y = plogis(eta))
+
+    eta <- eta[eta > 0]
+    plot(x = eta, y = plogis(eta))
+
+    plot(sort(eta))
+    mean(eta)
+
+    mean(r)
+    
+    tmp <- rVec(pm = pm, eta = abs(eta - mean(eta)))
     
     if(type == "high" | type == "low") {
         if(type == "low") eta <- -eta
@@ -80,11 +135,19 @@ simMissingness <- function(pm,
     }
     else if(type == "center" | type == "tails") {
         pm <- ifelse(type == "center", (1 - pm) / 2, pm / 2)
-        
+        pm
         tmp <- rVec(pm = pm, eta = eta)
-        r1 <- tmp$rVec
-        r2 <- rVec(pm = pm, eta = -eta, alpha = tmp$alpha)$rVec
+        mean(tmp$rVec)
+        r1  <- tmp$rVec
+        r2  <- rVec(pm = pm, eta = -eta, filter = !r1)$rVec
+        mean(r1)
+        mean(r2)
         
+        1 - mean(r1 | r2) 
+
+        mean(r)
+        mean(r1 & r2)
+       
         r <- r1 | r2
         if(type == "center") r <- !r
     }
